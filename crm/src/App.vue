7842f360af7e4f5ee13109756de1dcb2456302da
Egor <ytimanovskyi@betacom.com.pl>
2021-08-22 21:22:58 +0200

    created all the views

---
+++ crm/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <keep-alive>
+      <component :is="layout">
+        <router-view/>
+      </component>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+
+import EmptyLayout from './layouts/EmptyLayout';
+import MainLayout from './layouts/MainLayout';
+
+export default {
+  computed: {
+    layout() {
+      return (this.$route.meta.layout || 'empty') + '-layout'
+    }
+  },
+  components: {
+    EmptyLayout,
+    MainLayout,
+  }
+}
+</script>
+
+<style lang="scss">
+@import "~materialize-css/dist/css/materialize.min.css";
+@import "assets/index.css";
+</style>
