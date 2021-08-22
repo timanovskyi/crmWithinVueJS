e2f797c24acaeba434dfd6e373fdc4e34b7594e6
Egor <ytimanovskyi@betacom.com.pl>
2021-08-22 22:11:31 +0200

    mainLayout was divided on the 2 children. refactoring

--- crm/src/router/index.js
+++ crm/src/router/index.js
@@ -1,77 +1,61 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
     {
-        path: '/',
-        name: 'home',
-        component: Home
-    },
-    {
         path: '/login',
         name: 'login',
-        meta: {
-            layout: 'empty'
-        },
+        meta: {layout: 'empty'},
         component: () => import('../views/Login')
     },
     {
         path: '/register',
         name: 'register',
-        meta: {
-            layout: 'empty'
-        },
+        meta: {layout: 'empty'},
         component: () => import('../views/Register')
+    },
+    {
+        path: '/',
+        name: 'home',
+        meta: {layout: 'main'},
+        component: () => import('../views/Home')
     },
     {
         path: '/categories',
         name: 'categories',
-        meta: {
-            layout: 'main'
-        },
+        meta: {layout: 'main'},
         component: () => import('../views/Categories')
     },
     {
         path: '/detail-record',
         name: 'detail-record',
-        meta: {
-            layout: 'main'
-        },
+        meta: {layout: 'main'},
         component: () => import('../views/DetailRecord')
     },
     {
         path: '/history',
         name: 'history',
-        meta: {
-            layout: 'main'
-        },
+        meta: {layout: 'main'},
         component: () => import('../views/History')
     },
     {
         path: '/planning',
         name: 'planning',
-        meta: {
-            layout: 'main'
-        },
+        meta: {layout: 'main'},
         component: () => import('../views/Planning')
     },
     {
         path: '/profile',
         name: 'profile',
-        meta: {
-            layout: 'main'
-        },
+        meta: {layout: 'main'},
         component: () => import('../views/Profile')
     },
     {
         path: '/record',
         name: 'record',
-        meta: {
-            layout: 'main'
-        },
+        meta: {layout: 'main'},
         component: () => import('../views/Record')
     }
 ]
